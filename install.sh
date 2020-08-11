3611ddbad53fc110a4752b7c8a608e64d46b9b14
davidgonzalezfx <dvdramos16@gmail.com>
2020-08-10 22:53:04 -0500

    v2.0 beta

--- install.sh
+++ install.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 
 # install zsh
+echo -e "\033[95mapt-get update working..."
 sudo apt-get -y -qq update 2> /dev/null
-echo -e "\033[94mupdate finished"
+echo -e "\033[92mupdate finished"
+echo -e "\n----------\n"
+echo -e "\033[95mInstalling zsh..."
 sudo apt-get -y -qq install zsh
-echo -e "\033[94mzsh installed"
+echo -e "\033[92mzsh installed"
 sudo usermod -s /usr/bin/zsh $(whoami)
+echo -e "\n----------\n"
+echo -e "\033[95mInstalling Oh-My-Zsh..."
 
 # install oh-my-zsh
 sh -c "$(wget -q https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
--- install2.sh
+++ install2.sh
@@ -1,11 +1,67 @@
 #!/usr/bin/env bash
-# install auto-suggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
+
+echo -ne "\033[93mDavid mode (y/n): \033[0m"
+read mode
+
+if [ $mode == "y" ];
+then
+	new_prompt="container"
+	username="davidgonzalezfx"
+	email="dvdramos16@gmail.com"
+	colors="y"
+	plugins="y"
+else
+	echo -ne "\033[93mPlease type a new prompt: \033[0m"
+	read new_prompt
+
+	if [ -z $new_prompt ]
+	then
+		new_prompt="container"
+	fi
+
+	echo -ne "\033[93mDo you want enable colors (y/n): \033[0m"
+	read colors
+
+	echo -ne "\033[93mDo you want enable plugins (y/n): \033[0m"
+	read plugins
+
+	echo -ne "\033[93mPlease type your github username: \033[0m"
+	read username
+
+	if [ ! -z $username ]
+	then
+		echo "OK"
+	fi
+
+
+	echo -ne "\033[93mPlease type your github email: \033[0m"
+	read email
+
+	if [ ! -z $email ]
+	then
+		echo "OK"
+	fi	
+fi
+
+
+if [ $plugins == "y" ]
+then
+	# install auto-suggestions
+	git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+	# modify .zshrc
+	sed -i '71s/$/ \nplugins=(zsh-autosuggestions)/' ~/.zshrc
+	sed -i '74s/$/ \nsource ~\/.oh-my-zsh\/plugins\/git\/git.plugin.zsh/' ~/.zshrc
+	# sed -i -e '$asource /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh' ~/.zshrc
+	sed -i -e '$asource ~/.bash_aliases' ~/.zshrc
+fi
+
+if [ $colors == "y"]
+then
 # change prompt
 cat > ~/.oh-my-zsh/themes/robbyrussell.zsh-theme << EOF
 PROMPT="%(?:%{\$fg_bold[green]%}➜ :%{\$fg_bold[red]%}➜ )"
-PROMPT+=\$' %{\$fg[green]%}container %{\$fg[yellow]%}%c%{\$reset_color%} \$(git_prompt_info)\n'
+PROMPT+=\$' %{\$fg[green]%}\$new_prompt %{\$fg[yellow]%}%c%{\$reset_color%} \$(git_prompt_info)\n'
 PROMPT+='%(?:%{\$fg_bold[green]%}➜ :%{\$fg_bold[red]%}➜ ) '
 
 ZSH_THEME_GIT_PROMPT_PREFIX="%{\$fg_bold[red]%}(%{\$fg[red]%}"
@@ -13,6 +69,19 @@ ZSH_THEME_GIT_PROMPT_SUFFIX="%{\$reset_color%} "
 ZSH_THEME_GIT_PROMPT_DIRTY="%{\$fg[red]%}) %{\$fg[red]%}✗"
 ZSH_THEME_GIT_PROMPT_CLEAN="%{\$fg[red]%})"
 EOF
+else
+# Default Oh-My-Zsh theme
+cat > ~/.oh-my-zsh/themes/robbyrussell.zsh-theme << EOF
+PROMPT="%(?:%{$fg_bold[green]%}➜ :%{$fg_bold[red]%}➜ )"
+PROMPT+=' %{$fg[cyan]%}\$new_prompt $(git_prompt_info)'
+
+ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg_bold[blue]%}git:(%{$fg[red]%}"
+ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%} "
+ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[blue]%}) %{$fg[yellow]%}✗"
+ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg[blue]%})"
+EOF
+fi
+
 
 # set aliases
 cat > ~/.bash_aliases << EOF
@@ -23,21 +92,26 @@ alias lsd='ls -1d */'
 alias lsl='ls -1'
 EOF
 
-# modify .zshrc
-sed -i '71s/$/ \nplugins=(zsh-autosuggestions)/' ~/.zshrc
-sed -i '74s/$/ \nsource ~\/.oh-my-zsh\/plugins\/git\/git.plugin.zsh/' ~/.zshrc
-sed -i -e '$asource /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh' ~/.zshrc
-sed -i -e '$asource ~/.bash_aliases' ~/.zshrc
 
 # vim paste mode
 echo -e 'set pastetoggle=<F1>\n' > ~/.vimrc
 
 # git config
-git config --global user.email "dvdramos16@gmail.com"
-git config --global user.name "davidgonzalezfx" 
+git config --global user.email $email
+git config --global user.name $username
 git config --global credential.helper store
+echo -e '\033[92mYour git configuration:'
+git config --global --list
 
-echo -e "\033[94mplease execute \$source ~/.zshrc"
+# enable mysql
+status=service mysql status
+if [status -eq 3]
+then
+service mysql status
+fi
 
-# load files
-# source ~/.zshrc
+echo -e "\n----------\n"
+echo -e "\033[93mplease execute source ~/.zshrc"
+echo -e "\n----------\n"
+
+
