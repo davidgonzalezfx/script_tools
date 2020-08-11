bde702de07ac52cdca0113e27de45d08d5c0f7cc
davidgonzalezfx <dvdramos16@gmail.com>
2020-08-11 11:00:30 -0500

    v2.1 beta - prompt

--- install2.sh
+++ install2.sh
@@ -1,25 +1,15 @@
 #!/usr/bin/env bash
 
-
 echo -ne "\033[93mDavid mode (y/n): \033[0m"
 read mode
 
 if [ $mode == "y" ];
 then
-	new_prompt="container"
 	username="davidgonzalezfx"
 	email="dvdramos16@gmail.com"
 	colors="y"
 	plugins="y"
 else
-	echo -ne "\033[93mPlease type a new prompt: \033[0m"
-	read new_prompt
-
-	if [ -z $new_prompt ]
-	then
-		new_prompt="container"
-	fi
-
 	echo -ne "\033[93mDo you want enable colors (y/n): \033[0m"
 	read colors
 
@@ -35,23 +25,12 @@ else
 fi
 
 
-if [ $plugins == "y" ]
-then
-	# install auto-suggestions
-	git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-	# modify .zshrc
-	sed -i '71s/$/ \nplugins=(zsh-autosuggestions)/' ~/.zshrc
-	sed -i '74s/$/ \nsource ~\/.oh-my-zsh\/plugins\/git\/git.plugin.zsh/' ~/.zshrc
-	# sed -i -e '$asource /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh' ~/.zshrc
-	sed -i -e '$asource ~/.bash_aliases' ~/.zshrc
-fi
-
-if [ $colors == "y"]
+if [ $colors == "y" ]
 then
 # change prompt
 cat > ~/.oh-my-zsh/themes/robbyrussell.zsh-theme << EOF
 PROMPT="%(?:%{\$fg_bold[green]%}➜ :%{\$fg_bold[red]%}➜ )"
-PROMPT+=\$' %{\$fg[green]%}\$new_prompt %{\$fg[yellow]%}%c%{\$reset_color%} \$(git_prompt_info)\n'
+PROMPT+=\$' %{\$fg[green]%}container %{\$fg[yellow]%}%c%{\$reset_color%} \$(git_prompt_info)\n'
 PROMPT+='%(?:%{\$fg_bold[green]%}➜ :%{\$fg_bold[red]%}➜ ) '
 
 ZSH_THEME_GIT_PROMPT_PREFIX="%{\$fg_bold[red]%}(%{\$fg[red]%}"
@@ -63,7 +42,7 @@ else
 # Default Oh-My-Zsh theme
 cat > ~/.oh-my-zsh/themes/robbyrussell.zsh-theme << EOF
 PROMPT="%(?:%{\$fg_bold[green]%}➜ :%{\$fg_bold[red]%}➜ )"
-PROMPT+=' %{\$fg[cyan]%}\$new_prompt \$(git_prompt_info)'
+PROMPT+=' %{\$fg[cyan]%}container \$(git_prompt_info)'
 
 ZSH_THEME_GIT_PROMPT_PREFIX="%{\$fg_bold[blue]%}git:(\%{$fg[red]%}"
 ZSH_THEME_GIT_PROMPT_SUFFIX="%{\$reset_color%} "
@@ -82,6 +61,17 @@ alias lsd='ls -1d */'
 alias lsl='ls -1'
 EOF
 
+
+if [ $plugins == "y" ]
+then
+	# install auto-suggestions
+	git clone -q https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+	# modify .zshrc
+	sed -i '71s/$/ \nplugins=(zsh-autosuggestions)/' ~/.zshrc
+	sed -i '74s/$/ \nsource ~\/.oh-my-zsh\/plugins\/git\/git.plugin.zsh/' ~/.zshrc
+	# sed -i -e '$asource /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh' ~/.zshrc
+	sed -i -e '$asource ~/.bash_aliases' ~/.zshrc
+fi
 
 # vim paste mode
 echo -e 'set pastetoggle=<F1>\n' > ~/.vimrc
@@ -103,7 +93,7 @@ git config --global --list
 # enable mysql
 service mysql start
 
-echo -e "\n----------\n"
+echo -e "\n----------"
 echo -e "\033[93mplease execute source ~/.zshrc"
 echo -e "\n----------\n"
 
