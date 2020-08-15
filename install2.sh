8db55eb6d6107ee595dce994100889089986fb46
davidgonzalezfx <dvdramos16@gmail.com>
2020-08-15 13:33:49 -0500

    input new_prompt

--- install2.sh
+++ install2.sh
@@ -18,8 +18,18 @@ then
 	if [ $container == "y" ]
 	then
 		sed -i -e '$assh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDM5hW8axyzEYAmk7dDTm8ZpcqtymdHBgpkCj1arh4SO/jSvtgipQphOSrpU2QdQ7f73dPooKJU7T09sq7FDrtKZpnMtvccmqcIayXzUqdll59K/z5PSnk7PpbCFqudL6+omMa0+3jz4eCabdSJV7Krvc6fMuZDN5rjhrBbYcQgN+95/DNNeCyCc/topKoMOtnpxPZn2p9gLypV1FyT067TD7VrbZ5QJ7GxVwXyjkgTAEtyXPPj1/i3yCU+rovAC/lDbhCdw8m3Ejrzo8yMxNqB4TyMvtBdEPtIbJ0hz5pWSfnN9MWqa3Sodi07ytDADEWWcdvS8aLvKE7PXf4HwNZ3 vagrant@ubuntu-bionic' ~/.ssh/authorized_keys
+	fi
+	# set aliases
+cat > ~/.bash_aliases << EOF
+alias gfpush='git push -f origin "\$(git_current_branch)"'
+alias slog='git log --oneline --graph  --decorate'
+alias ccommits='git shortlog -s -n --all'
+alias lsd='ls -1d */'
+alias lsl='ls -1'
+EOF
+
 else
-	echo -ne "\033[93mDo you want enable colors (y/n): \033[0m"
+	echo -ne "\033[93mDo you want enable prompt colors (y/n): \033[0m"
 	read colors
 
 	echo -ne "\033[93mDo you want enable plugins (y/n): \033[0m"
@@ -33,6 +43,9 @@ else
 		
 fi
 
+echo -ne "\033[93mPlease type your new prompt: \033[0m"
+read new_prompt
+
 echo -ne "\033[93mDo you want enable MySQL (y/n): \033[0m"
 read sql
 
@@ -40,39 +53,26 @@ read sql
 if [ $colors == "y" ]
 then
 # change prompt
-cat > ~/.oh-my-zsh/themes/robbyrussell.zsh-theme << EOF
-PROMPT="%(?:%{\$fg_bold[green]%}➜ :%{\$fg_bold[red]%}➜ )"
-PROMPT+=\$' %{\$fg[green]%}container %{\$fg[yellow]%}%c%{\$reset_color%} \$(git_prompt_info)\n'
+echo "
+PROMPT=\"%(?:%{\$fg_bold[green]%}➜ :%{\$fg_bold[red]%}➜ )\"
+PROMPT+=\$' %{\$fg[green]%}$new_prompt %{\$fg[yellow]%}%c%{\$reset_color%} \$(git_prompt_info)\n'
 PROMPT+='%(?:%{\$fg_bold[green]%}➜ :%{\$fg_bold[red]%}➜ ) '
 
-ZSH_THEME_GIT_PROMPT_PREFIX="%{\$fg_bold[red]%}(%{\$fg[red]%}"
-ZSH_THEME_GIT_PROMPT_SUFFIX="%{\$reset_color%} "
-ZSH_THEME_GIT_PROMPT_DIRTY="%{\$fg[red]%}) %{\$fg[red]%}✗"
-ZSH_THEME_GIT_PROMPT_CLEAN="%{\$fg[red]%})"
-EOF
+ZSH_THEME_GIT_PROMPT_PREFIX=\"%{\$fg_bold[red]%}(%{\$fg[red]%}\"
+ZSH_THEME_GIT_PROMPT_SUFFIX=\"%{\$reset_color%} \"
+ZSH_THEME_GIT_PROMPT_DIRTY=\"%{\$fg[red]%}) %{\$fg[red]%}✗\"
+ZSH_THEME_GIT_PROMPT_CLEAN=\"%{\$fg[red]%})\" " > ~/.oh-my-zsh/themes/robbyrussell.zsh-theme
 else
 # Default Oh-My-Zsh theme
-cat > ~/.oh-my-zsh/themes/robbyrussell.zsh-theme << EOF
-PROMPT="%(?:%{\$fg_bold[green]%}➜ :%{\$fg_bold[red]%}➜ )"
-PROMPT+=' %{\$fg[cyan]%}container \$(git_prompt_info)'
+echo "
+PROMPT=\"%(?:%{\$fg_bold[green]%}➜ :%{\$fg_bold[red]%}➜ )\"
+PROMPT+=' %{\$fg[cyan]%}$new_prompt \$(git_prompt_info)'
 
-ZSH_THEME_GIT_PROMPT_PREFIX="%{\$fg_bold[blue]%}git:(\%{$fg[red]%}"
-ZSH_THEME_GIT_PROMPT_SUFFIX="%{\$reset_color%} "
-ZSH_THEME_GIT_PROMPT_DIRTY="%{\$fg[blue]%}) %{\$fg[yellow]%}✗"
-ZSH_THEME_GIT_PROMPT_CLEAN="%{\$fg[blue]%})"
-EOF
+ZSH_THEME_GIT_PROMPT_PREFIX=\"%{\$fg_bold[blue]%}git:(\%{$fg[red]%}\"
+ZSH_THEME_GIT_PROMPT_SUFFIX=\"%{\$reset_color%} \"
+ZSH_THEME_GIT_PROMPT_DIRTY=\"%{\$fg[blue]%}) %{\$fg[yellow]%}✗\"
+ZSH_THEME_GIT_PROMPT_CLEAN=\"%{\$fg[blue]%})\" " > ~/.oh-my-zsh/themes/robbyrussell.zsh-theme
 fi
-
-
-# set aliases
-cat > ~/.bash_aliases << EOF
-alias gfpush='git push -f origin "\$(git_current_branch)"'
-alias slog='git log --oneline --graph  --decorate'
-alias ccommits='git shortlog -s -n --all'
-alias lsd='ls -1d */'
-alias lsl='ls -1'
-EOF
-
 
 if [ $plugins == "y" ]
 then
@@ -96,7 +96,7 @@ then
 fi
 
 # vim paste mode
-echo -e 'set pastetoggle=<F1>\n' > ~/.vimrc
+echo -e 'set pastetoggle=<F2>\n' > ~/.vimrc
 
 # git config
 if [ ! -z $email ]
@@ -109,6 +109,7 @@ then
 	git config --global user.name $username
 fi
 git config --global credential.helper store
+
 echo -e '\033[92mYour git configuration:'
 git config --global --list
 
@@ -116,6 +117,7 @@ git config --global --list
 if [ $plugins == "y" ]
 then
 	sudo service mysql start
+fi
 
 echo -e "\n----------"
 echo -e "\033[93mplease execute source ~/.zshrc"
