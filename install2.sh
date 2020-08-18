278441e8b1c34b2a281f0a16b789d79be271cafa
davidgonzalezfx <dvdramos16@gmail.com>
2020-08-18 12:38:41 -0500

    File renamed and prompt fix

--- install2.sh
+++ install2.sh
@@ -57,7 +57,7 @@ then
 echo "
 PROMPT=\"%(?:%{\$fg_bold[green]%}➜ :%{\$fg_bold[red]%}➜ )\"
 PROMPT+=\$' %{\$fg[green]%}$new_prompt %{\$fg[yellow]%}%c%{\$reset_color%} \$(git_prompt_info)\n'
-PROMPT+='%(?:%{\$fg_bold[green]%}➜ :%{\$fg_bold[red]%}➜ ) '
+PROMPT+=\$'%(?:%{\$fg_bold[green]%}➜ :%{\$fg_bold[red]%}➜ )%{\$reset_color%} '
 
 ZSH_THEME_GIT_PROMPT_PREFIX=\"%{\$fg_bold[red]%}(%{\$fg[red]%}\"
 ZSH_THEME_GIT_PROMPT_SUFFIX=\"%{\$reset_color%} \"
@@ -69,7 +69,7 @@ echo "
 PROMPT=\"%(?:%{\$fg_bold[green]%}➜ :%{\$fg_bold[red]%}➜ )\"
 PROMPT+=' %{\$fg[cyan]%}$new_prompt \$(git_prompt_info)'
 
-ZSH_THEME_GIT_PROMPT_PREFIX=\"%{\$fg_bold[blue]%}git:(\%{$fg[red]%}\"
+ZSH_THEME_GIT_PROMPT_PREFIX=\"%{\$fg_bold[blue]%}git:(%{$fg[red]%}\"
 ZSH_THEME_GIT_PROMPT_SUFFIX=\"%{\$reset_color%} \"
 ZSH_THEME_GIT_PROMPT_DIRTY=\"%{\$fg[blue]%}) %{\$fg[yellow]%}✗\"
 ZSH_THEME_GIT_PROMPT_CLEAN=\"%{\$fg[blue]%})\" " > ~/.oh-my-zsh/themes/robbyrussell.zsh-theme
