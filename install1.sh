184e9a10ea20d724f914cc5abf9a3aeb3e373b29
David Gonzalez <dvdramos16@gmail.com>
2020-08-25 17:24:53 -0500

    Update install1.sh

--- install1.sh
+++ install1.sh
@@ -5,11 +5,14 @@ echo -e "\033[95mapt-get update working..."
 sudo apt-get -y -qq update 2> /dev/null
 echo -e "\033[92mupdate finished"
 echo -e "\n----------\n"
+
 echo -e "\033[95mInstalling neovim..."
 sudo apt install -y -qq software-properties-common
 sudo add-apt-repository ppa:neovim-ppa/unstable 
 sudo apt-get -y -qq update 2> /dev/null
 sudo apt-get -y -qq install neovim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 mkdir -p ~/.config/nvim
 echo "
 set runtimepath^=~/.vim runtimepath+=~/.vim/after
@@ -18,6 +21,7 @@ source ~/.vimrc
 " > ~/.config/nvim/init.vim
 echo -e "\033[92mnvim installed"
 echo -e "\n----------\n"
+
 echo -e "\033[95mInstalling zsh..."
 sudo apt-get -y -qq install zsh
 echo -e "\033[92mzsh installed"
