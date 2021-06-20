d8172d45474abad65f839ed163f04354c7832751
David Gonzalez <dvdramos16@gmail.com>
2021-06-20 17:56:14 -0500

    Feature - Install nvm and node

--- install1.sh
+++ install1.sh
@@ -19,6 +19,16 @@ cat init.vim > ~/.config/nvim/init.vim
 echo -e "\033[92mnvim installed\033[0m"
 echo -e "\n----------\n"
 
+echo -e "\033[95mInstalling nvm and node...\033[0m"
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
+source ~/.profile
+sed -i -e 'export NVM_DIR="$HOME/.nvm' ~/.zshrc
+sed -i -e '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm' ~/.zshrc
+sed -i -e '[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion'  ~/.zshrc
+nvm install --lts
+echo -e "\033[92mnvm and node installed\033[0m"
+echo -e "\n----------\n"
+
 echo -e "\033[95mInstalling zsh...\033[0m"
 sudo apt-get -y -qq install zsh
 echo -e "\033[92mzsh installed\033[0m"
