ec824cafd4cc1cda29c33a8f1b0081fa687fcfa2
David Gonzalez <dvdramos16@gmail.com>
2023-06-05 09:15:40 -0500

    Add(-): Promises column

--- r07.js
+++ r07.js
@@ -15,7 +15,6 @@ const createTable = () => {
 
   // Add the Bootstrap table class styles
   table.classList.add('table')
-  table.classList.add('table-striped')
   table.classList.add('table-bordered')
 
   const tableHeader = document.createElement('thead')
@@ -30,9 +29,13 @@ const createTable = () => {
   timeHeader.textContent = 'Time'
   headerRow.appendChild(timeHeader)
 
-  const fullTextHeader = document.createElement('th')
-  fullTextHeader.textContent = 'Notes'
-  headerRow.appendChild(fullTextHeader)
+  const notesTextHeader = document.createElement('th')
+  notesTextHeader.textContent = 'Notes'
+  headerRow.appendChild(notesTextHeader)
+
+  const promiseTextHeader = document.createElement('th')
+  promiseTextHeader.textContent = 'Promise'
+  headerRow.appendChild(promiseTextHeader)
 
   tableHeader.appendChild(headerRow)
   table.appendChild(tableHeader)
@@ -47,7 +50,6 @@ const createTable = () => {
 
 const buildTableRows = (data) => {
   for (const note of Object.values(data)) {
-    // Create a row and add the note data in the order: date, time, full text
     const itemRow = document.createElement('tr')
 
     const dateRow = document.createElement('td')
@@ -58,13 +60,17 @@ const buildTableRows = (data) => {
     timeRow.innerHTML = note.time
     itemRow.appendChild(timeRow)
 
-    const fullTextRow = document.createElement('td')
-    fullTextRow.innerHTML = note.full.replace(/->/g, '➜')
-    itemRow.appendChild(fullTextRow)
+    const notesTextRow = document.createElement('td')
+    notesTextRow.innerHTML = note.notes.replace(/->/g, '➜')
+    itemRow.appendChild(notesTextRow)
+
+    const promiseTextRow = document.createElement('td')
+    promiseTextRow.innerHTML = note.promises.substring(8)
+    itemRow.appendChild(promiseTextRow)
 
     // Add the row to the table
     const tableBody = document.getElementById('table-body')
-    tableBody.appendChild(itemRow)
+    if (tableBody) tableBody.appendChild(itemRow)
   }
 
   var tableCells = document.querySelectorAll('.table td')
@@ -78,25 +84,36 @@ const loadPromisesVerse = async (data) => {
     data
       .filter((e) => e.kind === 'highlight')
       .map(async (el) => {
-        const verse = await fetch(
-          'https://david-gonzalez-7e9dc6.netlify.app/.netlify/functions/api/ntv/' +
-            el.object.references[0].human.replace(/\s/g, '/').replace(/:/g, '/')
-        )
-        const verseJson = await verse.json()
+        try {
+          const regex = /(\D+)\s(\d+:\d+(?:-\d+)?)/
 
-        return {
-          date: new Date(el.object.created_dt).toLocaleDateString(),
-          time: new Date(el.object.created_dt).toLocaleTimeString(),
-          reference: el.object.references.map((r) => r.human).join(', '),
-          note: '<mark>' + verseJson.verse + '</mark>'
+          const verse = await fetch(
+            'https://david-gonzalez-7e9dc6.netlify.app/.netlify/functions/api/ntv/' +
+              el.object.references[0].human
+                .replace(regex, '$1/$2')
+                .replace(':', '/')
+                .replace(' ', '%20')
+          )
+          const verseJson = await verse.json()
+
+          return {
+            date: new Date(el.object.created_dt).toLocaleDateString(),
+            reference: el.object.references.map((r) => r.human).join(', '),
+            promise: '<mark>' + verseJson.verse + '</mark>'
+          }
+        } catch (e) {
+          return {
+            date: new Date(el.object.created_dt).toLocaleDateString(),
+            reference: el.object.references.map((r) => r.human).join(', '),
+            note: '<mark>404 ERROR</mark>'
+          }
         }
       })
   )
 }
 
 const fetchData = async () => {
-  loadBoostrap()
-
+  enableInfiniteScroll = false
   const url = `https://my.bible.com/users/DvdGonzalez/_cards.json?page=${currentPage++}`
   const response = await fetch(url)
   let data = await response.json()
@@ -110,31 +127,7 @@ const fetchData = async () => {
       note: el.object.content
     }))
 
-  notes = notes.filter((note) => {
-    const date = new Date(note.date)
-    const month = date.getMonth()
-    const year = date.getFullYear()
-
-    const today = new Date()
-    const currentMonth = today.getMonth()
-    const currentYear = today.getFullYear()
-
-    return month === currentMonth && year === currentYear
-  })
-
   let highlights = await loadPromisesVerse(data)
-
-  highlights = highlights.filter((highlight) => {
-    const date = new Date(highlight.date)
-    const month = date.getMonth()
-    const year = date.getFullYear()
-
-    const today = new Date()
-    const currentMonth = today.getMonth()
-    const currentYear = today.getFullYear()
-
-    return month === currentMonth && year === currentYear
-  })
 
   const notesMap = notes.concat(highlights).reduce((map, obj) => {
     const date = obj.date
@@ -146,18 +139,9 @@ const fetchData = async () => {
   }, {})
 
   for (const key in notesMap) {
-    notesMap[key]
-      .sort((a, b) => {
-        return new Date('1970/01/01 ' + a.time) - new Date('1970/01/01 ' + b.time)
-      })
-      .sort((a, b) => {
-        if (a.note.includes('Promise')) {
-          return 1
-        }
-        if (b.note.includes('Promise')) {
-          return -1
-        }
-      })
+    notesMap[key].sort((a, b) => {
+      return new Date('1970/01/01 ' + a.time) - new Date('1970/01/01 ' + b.time)
+    })
   }
 
   let mergedNotes = Object.values(notesMap).reduce((acc, curr) => {
@@ -166,27 +150,31 @@ const fetchData = async () => {
       const time = item.time
       const references = item.reference
       const notes = item.note
+      const promise = item.promise
 
       if (acc[date]) {
-        acc[date].time += `<br><br>${time}`
+        acc[date].time += !!time ? `<br><br>${time}` : ''
         acc[date].references += `<br><br>${references}`
-        acc[date].notes += `<br><br>${notes}`
-        acc[date].full += `<br><br>${references} - ${notes}`
+        acc[date].notes += !!notes ? `<br><br>${references} - ${notes}` : ''
+        acc[date].promises += !!promise ? `<br><br>${references} - ${promise}` : ''
         continue
       }
 
       acc[date] = {
         date: date,
-        time: time,
+        time: !!time ? time : '',
         references: references,
-        notes: notes,
-        full: `${references} - ${notes}`
+        notes: !!notes ? `${references} - ${notes}` : '',
+        promises: !!promise ? `${references} - ${promise}` : ''
       }
     }
 
     return acc
   }, {})
 
+  enableInfiniteScroll = true
+
+  console.log('enableInfiniteScroll', enableInfiniteScroll)
   return mergedNotes
 }
 
@@ -198,10 +186,11 @@ const createButton = () => {
   btn.addEventListener('click', async () => {
     loadBoostrap()
     document.getElementById('current-ui-view').innerHTML = ''
+    currentPage = 1
     const initialData = await fetchData()
     createTable()
     buildTableRows(initialData)
-    enableInfiniteScroll = true
+    btn.innerHTML = '<span>Reiniciar R07</span>'
   })
 
   const button = document.getElementsByClassName('yv-profile-link yv-text-ellipsis')[0]
@@ -215,7 +204,7 @@ window.addEventListener('load', function () {
 
 window.addEventListener('scroll', async () => {
   const { scrollHeight, scrollTop, clientHeight } = document.documentElement
-  if (scrollTop + clientHeight > scrollHeight - 700) {
+  if (enableInfiniteScroll && scrollTop + clientHeight > scrollHeight - 700) {
     const newData = await fetchData()
     buildTableRows(newData)
   }
